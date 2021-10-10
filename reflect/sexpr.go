ca698b213a1693fdac64c745ca289fce2639f024
lujet <462546997@qq.com>
2021-10-10 22:15:32 +0800

    S表达式

---
+++ reflect/sexpr.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"reflect"
+)
+
+// 结构被编码为一个关于字段绑定(field binding) 的列表，每个字段绑定都是一个
+// 两个元素的列表，其中第一个元素(使用符号) 是字段名，第二个元素是字段值，
+// map 也编码为元素对的列表，每个元素对都是map中一项的键和值。
+// S 表达式使用形式为(key . value) 的单个构造单元(cons cell) 来表示键值对,
+// 而不是用双元素的列表?
+
+func encode(buf *bytes.Buffer, v reflect.Value) error {
+	switch v.Kind() {
+	case reflect.Invalid:
+		buf.WriteString("nil")
+
+	case reflect.Int, reflect.Int8, reflect.Int16,
+		reflect.Int32, reflect.Int64:
+		fmt.Fprintf(buf, "%d", v.Int())
+
+	case reflect.Uint, reflect.Uint8, reflect.Uint16,
+		reflect.Uint32, reflect.Uint64:
+		fmt.Fprintf(buf, "%d", v.Uint())
+
+	case reflect.String:
+		fmt.Fprintf(buf, "%q", v.String())
+
+	case reflect.Ptr:
+		// 递归指针的值
+		return encode(buf, v.Elem())
+
+	case reflect.Array, reflect.Slice:
+		buf.WriteByte('(')
+		for i := 0; i < v.Len(); i++ {
+			if i > 0 {
+				buf.WriteByte(' ')
+			}
+			if err := encode(buf, v.Index(i)); err != nil {
+				return err
+			}
+		}
+		buf.WriteByte(')')
+
+	case reflect.Struct:
+		buf.WriteByte('(')
+		for i := 0; i < v.NumField(); i++ {
+			if i > 0 {
+				buf.WriteByte(' ')
+			}
+			fmt.Fprintf(buf, "(%s ", v.Type().Field(i).Name)
+			if err := encode(buf, v.Field(i)); err != nil {
+				return err
+			}
+			buf.WriteByte(')')
+		}
+		buf.WriteByte(')')
+
+	case reflect.Map:
+		buf.WriteByte('(')
+		for i, key := range v.MapKeys() {
+			if i > 0 {
+				buf.WriteByte(' ')
+			}
+			buf.WriteByte('(')
+			if err := encode(buf, key); err != nil {
+				return err
+			}
+			buf.WriteByte(' ')
+			if err := encode(buf, v.MapIndex(key)); err != nil {
+				return err
+			}
+			buf.WriteByte(')')
+		}
+		buf.WriteByte(')')
+	default: // float, complex, bool, chan, func, interface
+		return fmt.Errorf("unsupported type:%s", v.Type())
+	}
+	return nil
+}
